70e445f682dfee01e75ccb4d7019999f9743ca91
PeterFilip <peterfilip@outlook.com>
2021-10-23 00:39:25 -0400

    update intent classifier server, remove conncurrent.futures executor

--- intent_classification/server.py
+++ intent_classification/server.py
@@ -2,8 +2,6 @@ import asyncio
 import websockets
 from websockets.exceptions import ConnectionClosedOK, ConnectionClosed
 from classifier import IntentClassifier
-# from concurrent.futures import ProcessPoolExecutor
-
 
 
 # server needs to monitor mappings.json and look for changes, if found, retrain the model and put any incoming requests on hold
@@ -13,20 +11,6 @@ from classifier import IntentClassifier
 classifier = IntentClassifier()
 classifier.load()
 
-
-# async def echo(websocket, path):
-#     # await websocket.send(message)
-#     # x = await websocket.recv()
-#     with ProcessPoolExecutor() as executor:
-#         while True:
-#             # receive inquery from client
-#             client_inquery = await websocket.recv()
-
-#             # feed it through classifer
-#             future = executor.submit(classifier.query_intent, client_inquery)
-#             result = future.result()
-#             # send result back
-#             await websocket.send(result)
 
 
 async def echo(websocket, path):
